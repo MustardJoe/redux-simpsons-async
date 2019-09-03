0d39e9695c97fdb4eca9105cd3ba0a264c3d838f
Jonathan Forney <jonathan.forney@gmail.com>
2019-09-02 19:20:27 -0700

    reducer files comleted

---
+++ src/reducers/simpsonsReducer.js
@@ -0,0 +1,18 @@
+import { FETCH_QUOTE, FETCH_QUOTE_LOADING } from '../';
+
+const initialState = {
+  quoteDetails: {},
+  loading: false,
+  error: null
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case FETCH_QUOTE_LOADING:
+      return { ...state, loading: true };
+    case FETCH_QUOTE:
+      return { ...state, quoteDetails: action.payload, loading: false };
+    default:
+      return state;
+  }
+}
