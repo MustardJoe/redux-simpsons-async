2d0c1398a5ccbdba64e592f2746f6bfb711f3a8a
Jonathan Forney <jonathan.forney@gmail.com>
2019-09-02 19:22:55 -0700

    wrote quote container

---
+++ src/containers/QuoteContainers.js
@@ -0,0 +1,50 @@
+import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Quote from '../components/quote/Quote';
+import { getQuoteLoading, getName, getImgUrl, getQuote } from '../selectors/quoteSelectors';
+import { fetchQuote } from '../actions/simpsonsActions';
+import Load from '../components/quote/Load';
+
+class SimpsonsQuote extends Component{
+  static propTypes = {
+    character: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    quote: PropTypes.string.isRequired,
+    fetch: PropTypes.func.isRequired,
+    loading: PropTypes.bool.isRequired,
+  }
+
+  componentDidMount() {
+    this.props.fetch();
+    console.log('containters comp did mnt', this.props.fetch());
+  }
+
+  render() {
+    const { quote, character, image, loading, fetch } = this.props;
+    if(loading) return <h1>We are loading</h1>;
+    return (
+      <section>
+        <Load handleClick={fetch} /> 
+        <Quote quote={quote} character={character} image={image} />
+      </section>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  quote: getQuote(state),
+  character: getName(state),
+  image: getImgUrl(state),
+  loading: getQuoteLoading(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetch() {
+    dispatch(fetchQuote());
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SimpsonsQuote);
+
+
