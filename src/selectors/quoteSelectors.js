6ee997d2e714e5bb6d0ad890f9430e48ad2947c9
Jonathan Forney <jonathan.forney@gmail.com>
2019-09-02 19:26:47 -0700

    selectors in place

---
+++ src/selectors/quoteSelectors.js
@@ -0,0 +1,4 @@
+export const getQuote = state => state.simpsons.quoteDetails.quote;
+export const getName = state => state.simpsons.quoteDetails.character;
+export const getImgUrl = state => state.simpsons.quoteDetails.image;
+export const getQuoteLoading = state => state.simpsons.loading;
